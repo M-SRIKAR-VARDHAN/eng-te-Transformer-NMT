3e25bb52d2ee9696368f5df78a00a4bb1ecc36a6
Bellam Rahul <142339913+rahul-bellam@users.noreply.github.com>
2026-02-05 17:56:48 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
-
+#nmt
 class BilingualDataset(Dataset):
 
     def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len):
